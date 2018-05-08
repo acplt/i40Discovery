4c562629a37ae4f30b32aaf25fde51498ce23852
TorbenD <t.deppe@plt.rwth-aachen.de>
2018-05-08 11:12:02 +0200

    adding GetMessage and sendMessage

--- openAASDiscoveryServer/source/DiscoveryServer.c
+++ openAASDiscoveryServer/source/DiscoveryServer.c
@@ -25,9 +25,12 @@
 #include "libov/ov_macros.h"
 #include <pthread.h>
 #include "MessageSys_helpers.h"
+#include "libov/ov_logfile.h"
+#include "libov/ov_result.h"
+#include "libov/ov_path.h"
 
 #if OV_SYSTEM_NT
-#include <windows.h>
+	#include <windows.h>
 #endif
 
 #define MUTEX_LOCK(MUTEX)\
@@ -54,29 +57,51 @@ static void* thread_fcn(void*ptr){
 	OV_STRING JsonOutput = NULL;
 	OV_STRING errorMessage = NULL;
 	OV_UINT messageType = 0;
+	OV_RESULT resultOV = 0;
+	response_data responseData;
+	responseData.endpointExtern = NULL;
+	responseData.endpointIntern = NULL;
+	responseData.messageID = NULL;
 
-	// TODO JSON Encoding => Search for MessageType
+	// TODO JSON Encoding => Search for MessageType, protocolType, endpoints, messageID
 
 	switch(messageType){
 		case 1: // SecurityMessage
 			Ov_GetVTablePtr(openAASDiscoveryServer_Security, pvtableSecurity, &pthreadData->pDiscoveryServer->p_Security);
-			pvtableSecurity->m_getSecurityMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			if (pvtableSecurity)
+				resultOV = pvtableSecurity->m_getSecurityMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			else
+				ov_logfile_error("Could not get VTable Pointer of Security-Object");
 		break;
 		case 2: // RegistrationMessage
 			Ov_GetVTablePtr(openAASDiscoveryServer_Registration, pvtableRegistration, &pthreadData->pDiscoveryServer->p_Registration);
-			pvtableRegistration->m_getRegistrationMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			if (pvtableRegistration)
+				resultOV = pvtableRegistration->m_getRegistrationMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			else
+				ov_logfile_error("Could not get VTable Pointer of Registration-Object");
 		break;
 		case 3: // UnregistrationMessage
 			Ov_GetVTablePtr(openAASDiscoveryServer_Registration, pvtableRegistration, &pthreadData->pDiscoveryServer->p_Registration);
-			pvtableRegistration->m_getUnregistrationMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			if (pvtableRegistration)
+				resultOV = pvtableRegistration->m_getUnregistrationMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			else
+				ov_logfile_error("Could not get VTable Pointer of pvtableRegistration-Object");
 		break;
 		case 4: // SearchMessage
 			Ov_GetVTablePtr(openAASDiscoveryServer_Search, pvtableSearch, &pthreadData->pDiscoveryServer->p_Search);
-			pvtableSearch->m_getSearchMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			if (pvtableSearch)
+				resultOV = pvtableSearch->m_getSearchMessage(pthreadData->message, &JsonOutput, &errorMessage);
+			else
+				ov_logfile_error("Could not get VTable Pointer of Search-Object");
 		break;
 		default:
 		break;
 	}
+
+	// Check ErrorMessage
+	if (resultOV)
+		ov_logfile_error("Error in part-function: %s", errorMessage);
+
 	// freeMemory and delete thread from list
 	for (OV_UINT i = 0; i < pthreadData->pDiscoveryServer->v_threadDataHndl.veclen; i++){
 		if ((OV_UINT)pthreadData == pthreadData->pDiscoveryServer->v_threadDataHndl.value[i]){
@@ -90,6 +115,20 @@ static void* thread_fcn(void*ptr){
 	}
 
 	ov_string_setvalue(&pthreadData->message, NULL);
+
+	// Send Response
+	ov_string_setvalue(&errorMessage, NULL);
+	resultOV = openAASDiscoveryServer_DiscoveryServer_sendMessage(pthreadData->pDiscoveryServer, JsonOutput, &errorMessage, responseData);
+	if (resultOV){
+		ov_logfile_error("Error in sendMessage: %s", errorMessage);
+		ov_string_setvalue(&errorMessage, NULL);
+	}
+
+	// freeMemory
+	ov_string_setvalue(&JsonOutput, NULL);
+	ov_string_setvalue(&responseData.endpointExtern, NULL);
+	ov_string_setvalue(&responseData.endpointIntern, NULL);
+	ov_string_setvalue(&responseData.messageID, NULL);
 	Ov_HeapFree(pthreadData);
 
 	pthread_exit(0);
@@ -146,7 +185,7 @@ OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_getMessage(OV_I
 	threadResult = pthread_create(&pthreadData->thread, NULL, thread_fcn, (void*) pthreadData);
 
 	if (threadResult){
-		ov_logfile_info("Could not create a thread for getting Message. ErrorCode: %i", threadResult);
+		ov_logfile_error("Could not create a thread for getting Message. ErrorCode: %i", threadResult);
 		ov_string_print(errorMessage, "Could not create a thread for getting Message. ErrorCode: %i", threadResult);
 		Ov_SetDynamicVectorLength(&pinst->v_threadDataHndl, pinst->v_threadDataHndl.veclen-1, INT);
 		ov_string_setvalue(&pthreadData->message, NULL);
@@ -157,20 +196,41 @@ OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_getMessage(OV_I
 	return OV_ERR_OK;
 }
 
-OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_sendMessage(OV_INSTPTR_openAASDiscoveryServer_DiscoveryServer pinst, const OV_STRING JsonInput, OV_STRING *errorMessage) {
-/*
+OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_sendMessage(OV_INSTPTR_openAASDiscoveryServer_DiscoveryServer pinst, const OV_STRING JsonInput, OV_STRING *errorMessage, const response_data responseData) {
+
 	OV_STRING tmpString = NULL;
 	OV_INSTPTR_MessageSys_Message panswerMessage = NULL;
 	OV_RESULT resultOV = 0;
-	OV_UINT protocollType = 0;
-	*errorMessage = NULL;
+	if (!JsonInput){
+		ov_string_print(errorMessage, "empty JSON-String");
+		return OV_ERR_GENERIC;
+	}
 
-	switch (protocollType){
-		case 1: // MessageSys
+	switch (responseData.protocolType){
+		case 1: { // MessageSys
+			// Parse endpoint
+			OV_STRING *pListExtern = NULL;
+			OV_UINT len = 0;
+			// endpoint have to be of format IP:MANAGERNAME:PathToKSEndpoint
+			pListExtern = ov_string_split(responseData.endpointExtern, ":", &len);
+			if (len != 3){
+				ov_logfile_error("EndpointExtern is not of correct format");
+				ov_string_print(errorMessage, "EndpointExtern is not of correct format");
+				return OV_ERR_GENERIC;
+			}
+
+			OV_STRING *pListIntern = NULL;
+			// endpoint have to be of format IP:MANAGERNAME:PathToKSEndpoint
+			pListIntern = ov_string_split(responseData.endpointIntern, ":", &len);
+			if (len != 3){
+				ov_logfile_error("EndpointIntern is not of correct format");
+				ov_string_print(errorMessage, "EndpointIntern is not of correct format");
+				return OV_ERR_GENERIC;
+			}
+
 			// Create MessageObject in Outbox
-			ov_string_setvalue(&tmpString, NULL);
-			ov_string_setvalue(&tmpString, "answerMessageTo");
-			// TODO adding number for responsing message ov_string_append(&tmpString, xxx);
+			ov_string_setvalue(&tmpString, "answerMessageTo_");
+			ov_string_append(&tmpString, responseData.messageID);
 			resultOV = Ov_CreateObject(MessageSys_Message, panswerMessage, &pinst->p_ComponentManager.p_OUTBOX, tmpString);
 			ov_string_setvalue(&tmpString, NULL);
 			if(Ov_Fail(resultOV)){
@@ -179,18 +239,20 @@ OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_sendMessage(OV_
 				return OV_ERR_GENERIC;
 			}
 
-			// TODO: Sender = Receiver of old message
-			//MessageSys_Message_senderAddress_set(panswerMessage, message->v_receiverAddress);
-			//MessageSys_Message_senderName_set(panswerMessage, message->v_receiverName);
-			//MessageSys_Message_senderComponent_set(panswerMessage, message->v_receiverComponent);
+			// Sender = Endpoint of Discovery-Server (IP, MANAGER-Name, Path)
+			MessageSys_Message_senderAddress_set(panswerMessage, pListIntern[0]);
+			MessageSys_Message_senderName_set(panswerMessage, pListIntern[1]);
+			MessageSys_Message_senderComponent_set(panswerMessage, pListIntern[2]);
+			ov_string_freelist(&pListIntern);
 
-			// TODO: Receiver = Sender of old message
-			//MessageSys_Message_receiverAddress_set(panswerMessage, message->v_senderAddress);
-			//MessageSys_Message_receiverName_set(panswerMessage, message->v_senderName);
-			//MessageSys_Message_receiverComponent_set(panswerMessage, message->v_senderComponent);
+			// Receiver = Sender of old message
+			MessageSys_Message_receiverAddress_set(panswerMessage, pListExtern[0]);
+			MessageSys_Message_receiverName_set(panswerMessage, pListExtern[1]);
+			MessageSys_Message_receiverComponent_set(panswerMessage, pListExtern[2]);
+			ov_string_freelist(&pListExtern);
 
 			// TODO: reference Msg
-			// ov_string_setvalue(&panswerMessage->v_refMsgID, message->v_msgID);
+			ov_string_setvalue(&panswerMessage->v_refMsgID, responseData.messageID);
 
 			// Body
 			// XML Encoding
@@ -203,7 +265,6 @@ OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_sendMessage(OV_
 
 			// Message ready for sending
 			panswerMessage->v_msgStatus = MSGREADYFORSENDING;
-
 
 			// Search for MsgDelivery object
 			OV_INSTPTR_MessageSys_MsgDelivery pmsgDelivery = NULL;
@@ -226,13 +287,14 @@ OV_DLLFNCEXPORT OV_RESULT openAASDiscoveryServer_DiscoveryServer_sendMessage(OV_
 				ov_string_setvalue(errorMessage, "Could not send Message");
 				return OV_ERR_GENERIC;
 			}
+		}
 		break;
 		case 2: // OPC UA
 		break;
 		case 3: // KS http
 		break;
 	}
-*/
+
     return OV_ERR_OK;
 }
 
